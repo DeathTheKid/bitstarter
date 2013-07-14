27cb3dcec6d83e1c1460bfbbe18db99e04baa84f
Brian Wimpsett <algorhythm511@live.com>
2013-07-14 03:03:28 +0000

    Made Change to Web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buff = fs.readFileSync('index.html');
+  var message = buff.toString();
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
